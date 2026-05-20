2f1974ae2e68cf2fe7b6b97df5895b71a3fabf80
kc505 <lydiekasey123@gmail.com>
2026-05-20 13:39:11 +0100

    Add authentication system with client and provider roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -46,4 +48,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function isProvider(): bool
+{
+    return $this->role === 'provider';
+}
+
+public function isClient(): bool
+{
+    return $this->role === 'client';
+}
 }
